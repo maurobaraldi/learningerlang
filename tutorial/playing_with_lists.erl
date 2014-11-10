b030db7a83e39e29a9066ab1065530034ee29812
Mauro Baraldi <mbaraldi@mwneo.com>
2014-11-10 15:07:38 -0200

    Adding lists features basic skils

---
+++ tutorial/playing_with_lists.erl
@@ -0,0 +1,5 @@
+[M1|T1] = [malibu, curitiba, london, shinzeng].
+M1.
+T1.
+L1 = [madrid|T1].
+L1.
---
+++ tutorial/reversinglists.erl
@@ -0,0 +1,10 @@
+-module(reversinglists).
+-export([reverse/1]).
+
+reverse(List) ->
+    reverse(List, []).
+
+reverse([Head | Rest], Reversed_List) ->
+    reverse(Rest, [Head | Reversed_List]);
+reverse([], Reversed_List) ->
+    Reversed_List.
